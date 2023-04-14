b9719c03e369c6461a87c75f8d53c06e69230f56
gustavojms <gustavo.joset0@hotmail.com>
2023-04-13 22:27:18 -0300

    Creating spring project

---
+++ src/main/java/dev/chat/demo/DevChatApplication.java
@@ -0,0 +1,13 @@
+package dev.chat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/chat/demo/DevChatApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.chat.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
